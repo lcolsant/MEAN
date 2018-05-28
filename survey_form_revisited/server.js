e85dec4dd0367d0d2204fed1d8d600bffe3f4a77
lcolsant <lee.colsant@gmail.com>
2018-05-28 13:09:44 -0700

    adding new files

---
+++ survey_form_revisited/server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 8000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname + "/static")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+const server = app.listen(port, ()=> console.log(`listening on port ${port}`));
+const io = require('socket.io')(server);
+
+// io.on('connection', function (socket) {
+  
+//         socket.emit('greeting', { msg: 'Greetings, from server Node, brought to you by Sockets! -Server' });
+//         socket.on('thankyou', function (data) {
+//         console.log(data.msg); 
+//     });
+// });
+
+//listen to connection even from the client side
+io.sockets.on('connection', function (socket){
+    //server listens to "posting_form" event
+    socket.on("posting_form", function (data){
+        //generate a random number
+        var random_number = Math.floor((Math.random() * 1000) + 1);
+        //will emit the data to the client
+        socket.emit('updated_message', {response: data}); 
+        socket.emit('random_number', {response: random_number}); 
+    });
+});
+      
+
+  //var session = require('express-session');
+
+// app.use(session({
+//   secret: 'thisissecret',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { maxAge: 60000 }
+// }));
+
+
+app.get("/", function (request, response){
+
+    response.render('index');
+})
+
+// app.post("/users", function (request, response){
+
+//     var data = {
+//         fname:request.body.name,
+//         location:request.body.location,
+//         language:request.body.language,
+//         comment:request.body.desc,
+
+//     }
+
+//     response.render('index',{data:data});
+// })
+
+app.post("/return", function (request, response){
+    response.redirect('/');
+})
+
+
+
+
