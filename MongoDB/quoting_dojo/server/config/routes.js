7550f5a14a55995cf824e438abbdb73c6b8e0cff
lcolsant <lee.colsant@gmail.com>
2018-06-11 14:41:21 -0700

    added controllers

--- MongoDB/quoting_dojo/server/config/routes.js
+++ MongoDB/quoting_dojo/server/config/routes.js
@@ -1,41 +1,22 @@
 const mongoose = require('mongoose')
 const Quote = mongoose.model('Quotes');
+const control = require('../controllers/quotes.js');
 
 module.exports = function(app){
 
     app.get('/', function(req, res){
     
-        res.render('index');
+        control.index(req, res);
     });
     
     app.post('/process', function(req, res){
-        console.log('Post Data:', req.body);
-        
-        Quote.create(req.body, function(err){
-            if(err){
-                console.log(err);
-                for(var key in err.errors){
-                    req.flash('registration', err.errors[key].message);
-                }
-            }else{
-                console.log('Successfully added quote.');
-                res.redirect('/quotes');
-            }
-        });
+     
+        control.process(req, res);
 
     });
     
     app.get('/quotes', function(req,res){
        
-        Quote.find({},function(err,quotes){
-            if(err){
-                console.log('Something went wrong...');
-                res.status(500).send('Something went wrong...'); 
-            }else{
-                console.log('successfully retrieved quotes from the DB!');
-                console.log('quotes object:',quotes);
-                res.render('quotes',{quotes:quotes});
-            }
-        })
+        control.quotes(req, res);
     })
 }
---
+++ MongoDB/quoting_dojo/server/controllers/quotes.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Quote = mongoose.model('Quotes');
+
+module.exports = {
+    
+    index: function(req,res){
+        res.render('index');
+    },
+
+
+    process: function(req, res){
+        console.log('Post Data:', req.body);
+        
+        Quote.create(req.body, function(err){
+            if(err){
+                console.log(err);
+                for(var key in err.errors){
+                    req.flash('registration', err.errors[key].message);
+                }
+            }else{
+                console.log('Successfully added quote.');
+                res.redirect('/quotes');
+            }
+        });
+    },
+
+    quotes: function(req, res){
+
+
+        Quote.find({},function(err,quotes){
+            if(err){
+                console.log('Something went wrong...');
+                res.status(500).send('Something went wrong...'); 
+            }else{
+                console.log('successfully retrieved quotes from the DB!');
+                console.log('quotes object:',quotes);
+                res.render('quotes',{quotes:quotes});
+            }
+        })
+    },
+
+}
