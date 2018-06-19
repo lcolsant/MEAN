565c82a7209aa8efbd20811dbda8cbb28a83addb
lcolsant <lee.colsant@gmail.com>
2018-06-19 14:36:22 -0700

    adding registration-form app

---
+++ Angular/registration-form/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { User } from './user'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  dataSubmitted: boolean = false;
+  //instantiate new user object
+  user = new User();
+
+  //declare variables that will capture and store last user data
+  user_name: string = "";
+  user_email: string = "";
+
+  onSubmit(event: Event, formData: NgForm){
+    //prevent page from refreshing after form submit
+    event.preventDefault();
+    console.log('form submitted');
+    console.log(this.user);
+
+    //store user data to be passed back to form for success message
+    this.user_name = this.user.fname;
+    this.user_email = this.user.email;
+
+    //refresh form
+    formData.reset();
+  };
+
+}
---
+++ Angular/registration-form/src/app/user.ts
@@ -0,0 +1,14 @@
+
+//defint and export User class
+  export class User {
+    fname: string;
+    lname: string;
+    email: string;
+    password: string;
+    passConfirm: string;
+    add1: string;
+    add2: string;
+    city: string;
+    state: string;
+    lucky: boolean;
+  }
